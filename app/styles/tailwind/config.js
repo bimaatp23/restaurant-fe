18841c1eed4e84baddd777c72f434736478942b2
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-04-16 09:58:24 +0700

    Setup tailwindcss

---
+++ app/styles/tailwind/config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.hbs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const autoprefixer = require('autoprefixer');
+const tailwind = require('tailwindcss');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
+    postcssOptions: {
+      compile: {
+        // track changes in template, css, scss, and tailwind config files
+        cacheInclude: [/.*\.(css|scss|hbs)$/, /.tailwind\/config\.js$/],
+        plugins: [
+          {
+            module: autoprefixer,
+            options: {},
+          },
+          {
+            module: tailwind,
+            options: {
+              config: './app/styles/tailwind/config.js',
+            },
+          },
+        ],
+      },
+    },
 
     // Add options here
   });
