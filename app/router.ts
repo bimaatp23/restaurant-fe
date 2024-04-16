653a4dad53b78d9a0569c38d5997c02aa94d2166
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-04-16 22:50:30 +0700

    Create home route

--- app/router.ts
+++ app/router.ts
@@ -1,11 +1,11 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'restaurant-fe/config/environment';
+import EmberRouter from '@ember/routing/router'
+import config from 'restaurant-fe/config/environment'
 
 export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
+  location = config.locationType
+  rootURL = config.rootURL
 }
 
 Router.map(function () {
-  // Add route declarations here
-});
+  this.route('home', { path: '/' })
+})
