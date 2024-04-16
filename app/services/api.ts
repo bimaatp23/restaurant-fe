60750105368b016216d36ec26d1d47e2e5cd9531
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-04-16 22:46:10 +0700

    Create api service

---
+++ app/services/api.ts
@@ -0,0 +1,21 @@
+import Service from '@ember/service'
+
+export default class ApiService extends Service {
+  endpoint: string = 'http://localhost:3000'
+
+  async getBasic(url: string): Promise<any> {
+    return await fetch(this.endpoint + '/api/' + url)
+      .then((response) => response.json())
+      .then((json) => json)
+  }
+}
+
+// Don't remove this declaration: this is what enables TypeScript to resolve
+// this service using `Owner.lookup('service:api')`, as well
+// as to check when you pass the service name as an argument to the decorator,
+// like `@service('api') declare altName: ApiService`.
+declare module '@ember/service' {
+  interface Registry {
+    api: ApiService
+  }
+}
