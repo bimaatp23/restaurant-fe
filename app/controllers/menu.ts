f8ac12a35680c2db95c99522aac9cf49b96d0825
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-04-18 21:33:06 +0700

    Create menu controller

---
+++ app/controllers/menu.ts
@@ -0,0 +1,24 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import type RouterService from '@ember/routing/router-service';
+import { service } from '@ember/service';
+import type SessionService from 'restaurant-fe/services/session';
+
+export default class MenuController extends Controller {
+    @service session!: SessionService;
+    @service router!: RouterService;
+
+    orderButtonText: string = this.session.isLogin()
+        ? this.session.isAdmin()
+            ? 'Edit'
+            : 'Add to Cart'
+        : 'Order Now';
+
+    @action
+    orderButtonAction() {
+        if (this.session.isLogin()) {
+        } else {
+            this.router.transitionTo('login');
+        }
+    }
+}
