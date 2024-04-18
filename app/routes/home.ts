2cd9ce3c404dfeb21df4148da1bcc1ab654a07f4
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-04-18 19:53:27 +0700

    Create menu route

--- app/routes/home.ts
+++ app/routes/home.ts
@@ -1,27 +1,3 @@
-import { action } from '@ember/object';
 import Route from '@ember/routing/route';
-import { service } from '@ember/service';
-import type { Menu } from 'menu/Menu';
-import type MenuService from 'restaurant-fe/services/menu';
-import RSVP from 'rsvp';
 
-export default class HomeRoute extends Route {
-    @service menu!: MenuService;
-
-    menuList: Menu[] = [];
-
-    @action
-    async getMenu(): Promise<void> {
-        await this.menu.getMenu().then((response) => {
-            this.menuList = response.output_schema;
-        });
-    }
-
-    async model() {
-        await this.getMenu();
-
-        return RSVP.hash({
-            menuList: this.menuList,
-        });
-    }
-}
+export default class HomeRoute extends Route {}
---
+++ app/routes/menu.ts
@@ -0,0 +1,27 @@
+import { action } from '@ember/object';
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import type { Menu } from 'menu/Menu';
+import type MenuService from 'restaurant-fe/services/menu';
+import RSVP from 'rsvp';
+
+export default class MenuRoute extends Route {
+    @service menu!: MenuService;
+
+    menuList: Menu[] = [];
+
+    @action
+    async getMenu(): Promise<void> {
+        await this.menu.getMenu().then((response) => {
+            this.menuList = response.output_schema;
+        });
+    }
+
+    async model() {
+        await this.getMenu();
+
+        return RSVP.hash({
+            menuList: this.menuList,
+        });
+    }
+}
