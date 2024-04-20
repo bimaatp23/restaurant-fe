dcc588ae5b8fe1130c6cb3069771aeaf9afa3b48
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-04-20 20:36:47 +0700

    Update constant

--- app/Constant.ts
+++ app/Constant.ts
@@ -1,4 +1,8 @@
 export default class Constant {
     public static ROLE_ADMIN = 'admin';
     public static ROLE_CUSTOMER = 'customer';
+
+    public static STATUS_PENDING = 'PENDING';
+    public static STATUS_PROCESS = 'PROCESS';
+    public static STATUS_DONE = 'DONE';
 }
